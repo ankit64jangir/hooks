388f2fb75d852cec8032dc0494a4c6a4f982395c
Ankit Jangir <55452218+ankit64jangir@users.noreply.github.com>
2021-06-21 21:32:34 +0530

    change in useReducer2 file

--- src/components/UseReducer2.js
+++ src/components/UseReducer2.js
@@ -1,13 +1,17 @@
 import React, { useState, useReducer } from "react";
 
-const intialValue = 0;
+const intialValue = [
+  { id: Date.now(), name: "Ankit", email: "ankit@gmail.com" }
+];
 
 function reducer(state, action) {
   switch (action.type) {
-    case "increment":
-      return state + 1;
-    case "decrement":
-      return state - 1;
+    case "add":
+      return [...state, action.payload];
+    case "delete":
+      return state.filter((contact) => {
+        return contact.id !== action.payload.id;
+      });
     default:
       break;
   }
@@ -15,13 +19,62 @@ function reducer(state, action) {
 
 export default function UseReducer1() {
   const [state, dispatch] = useReducer(reducer, intialValue);
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+
+  console.log(state);
+
+  const addContact = (e) => {
+    e.preventDefault();
+
+    const contact = {
+      id: Date.now(),
+      name: name,
+      email: email
+    };
+    dispatch({ type: "add", payload: contact });
+
+    setName("");
+    setEmail("");
+  };
 
   return (
     <div className="App">
       <h1>useReducer 2</h1>
-      <h2>{state}</h2>
-      <button onClick={() => dispatch({ type: "increment" })}>Increment</button>
-      <button onClick={() => dispatch({ type: "decrement" })}>Decrement</button>
+      <form onSubmit={addContact}>
+        <input
+          type="text"
+          placeholder="Name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <br />
+        <input
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <br />
+        <button type="submit">Add contact</button>
+      </form>
+      <br />
+      <div>
+        {state.map((contact) => {
+          return (
+            <div>
+              {contact.name} : {contact.email}
+              <button
+                onClick={() =>
+                  dispatch({ type: "delete", payload: { id: contact.id } })
+                }
+              >
+                Delete
+              </button>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
