7e5f8ba8f4a508cd38722205eb8f369dbdd157da
Ankit Jangir <55452218+ankit64jangir@users.noreply.github.com>
2021-06-21 20:52:52 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./styles.css";
+import UseReducer1 from "./components/UseReducer1";
+import UseReducer2 from "./components/UseReducer2";
+
+export default function App() {
+  return (
+    <div className="App">
+      <UseReducer1 />
+      <hr />
+      <UseReducer2 />
+    </div>
+  );
+}
---
+++ src/components/UseReducer1.js
@@ -0,0 +1,36 @@
+import React, { useState, useReducer } from "react";
+
+const intialValue = 0;
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "increment":
+      return state + 1;
+    case "decrement":
+      return state - 1;
+    default:
+      break;
+  }
+}
+
+export default function UseReducer1() {
+  // const [counter, setCounter] = useState(0);
+  const [state, dispatch] = useReducer(reducer, intialValue);
+
+  // const increment = () => {
+  //   setCounter(counter + 1);
+  // };
+
+  // const decrement = () => {
+  //   setCounter(counter - 1);
+  // };
+
+  return (
+    <div className="App">
+      <h1>useReducer 1</h1>
+      <h2>{state}</h2>
+      <button onClick={() => dispatch({ type: "increment" })}>Increment</button>
+      <button onClick={() => dispatch({ type: "decrement" })}>Decrement</button>
+    </div>
+  );
+}
